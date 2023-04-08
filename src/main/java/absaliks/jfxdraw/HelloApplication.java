c8184023f5acf5f03be9c94d3be17ea76deafa07
Shamil Absalikov <13360483+absaliks@users.noreply.github.com>
2023-04-08 12:49:34 +0300

    Borderless graphics (not click trough)

--- src/main/java/absaliks/jfxdraw/HelloApplication.java
+++ src/main/java/absaliks/jfxdraw/HelloApplication.java
@@ -3,21 +3,41 @@ package absaliks.jfxdraw;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage primaryStage) {
+        // Create a new rectangle
+        Rectangle rectangle = new Rectangle(50, 50, 100, 100);
+        rectangle.setFill(Color.RED);
+
+        // Create a new pane and add the rectangle to it
+        Pane root = new Pane();
+        root.getChildren().add(rectangle);
+
+        // Create a new scene and add the pane to it
+        Scene scene = new Scene(root);
+
+        // Create a new stage and set the scene
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
+
+        // Make the stage always on top
+        stage.setAlwaysOnTop(true);
+
+        // Show the stage
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
